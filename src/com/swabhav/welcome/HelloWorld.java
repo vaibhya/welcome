f1fa294a5a6ebf06a67c0ca45a50c4cb42c922dc
vaibhya <vk751999@gmail.com>
2021-03-18 16:42:43 +0530

    1st commit

---
+++ src/com/swabhav/welcome/HelloWorld.java
@@ -0,0 +1,15 @@
+package com.swabhav.welcome;
+
+import java.util.Scanner;
+
+public class HelloWorld {
+//	Scanner sc = new Scanner (System.in);
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner (System.in);
+		int a = sc.nextInt();
+		System.out.println("Hello Vaibhav"+a);
+
+	}
+
+}
